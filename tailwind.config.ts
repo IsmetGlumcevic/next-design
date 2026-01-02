2d26e692ffa0bf5fde7141d99427504395a0c634
Ismet Glumcevic <ismet.glumcevic@gmail.com>
2026-01-02 11:42:08 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import preset from "aleta-ui/tailwind-preset";
 
 const config: Config = {
+  presets: [preset],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
